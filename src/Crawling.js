b8cc4355dbbd2bfe0a4d9ba2459228ce450c31e7
seungmin-dev <seungmin.dev@gmail.com>
2021-04-06 13:33:49 +0900

    Practicing with Youtube Api

--- src/Crawling.js
+++ src/Crawling.js
@@ -1,15 +1,12 @@
-// import React from "react";
 const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
-
-// const Crawling = () => {
-    
-//   return 
-// }
+const fetch = require("node-fetch");
+const getYoutubeApi = require("./api");
 
 var resultList = [];
 var cnt = 0;
+var data = "";
 
 const delay = (ms) => {
     return new Promise(function(resolve, reject) {
@@ -29,31 +26,39 @@ const getHTML = (url) => {
 }
 
 const main = () => {
-    fs.readFile('sample.txt', 'utf8', function(err, data) {
-        var allText = data;
-        var list = allText.split('\n');
-        var result = [];
-        for (let i = 0; i < list.length-1; i++) {
-            result.push(list[i].split('^')[4]);
-        }
-        for (let j = 0; j < result.length; j++) {
-            getHTML(result[j]).then(html => {
-                let result = {};
-                const $ = cheerio.load(html.data);
-                result['title'] = $("body").find(".search_tit").text();
-                return result;
-            }).then(res => {
-                cnt++;
-                resultList.push(res);
-                if(result.length == cnt) {
-                    fs.writeFile('result_json.txt', JSON.stringify(resultList), 'utf8', function(error) {
-                        console.log('write end');
-                    });
-                }
-            })
-                
-        }
-    });
+    getYoutubeApi();
+
+    // /https://developers.google.com/youtube/v3/docs/search/list?hl=ko#javascript
+
+    // var items = data.items;
+    // console.log('items:',items);
+    // var itemArr = [];
+    // for (let index = 0; index < items.length; index++) {
+    //     const element = array[index];
+        
+    // }
+    // var list = allText.split('\n');
+    //var list = items.split(',');
+    var result = [];
+    // for (let i = 0; i < list.length-1; i++) {
+    //     result.push(list[i].split('^')[4]);
+    // }
+    // for (let j = 0; j < result.length; j++) {
+    //     getHTML(result[j]).then(html => {
+    //         let result = {};
+    //         const $ = cheerio.load(html.data);
+    //         result['title'] = $("body").find(".search_tit").text();
+    //         return result;
+    //     }).then(res => {
+    //         cnt++;
+    //         resultList.push(res);
+    //         if(result.length == cnt) {
+    //             fs.writeFile('youtube_result.txt', JSON.stringify(resultList), 'utf8', function(error) {
+    //                 console.log('write end');
+    //             });
+    //         }
+    //     });
+    // }
 }
 
 main();
