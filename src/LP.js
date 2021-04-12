cec3f5c9a5220ca6aade2d8cbd3fc7c3a4ce52d7
seungmin_dev <seungmin.dev@gmail.com>
2021-04-13 06:46:16 +0900

    Get the source from youtube

--- src/LP.js
+++ src/LP.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 const LP = ({itemArr, lpImg, musicUrl}) => {
+    const getId = (url) => {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        var match = url.match(regExp);
+    
+        if (match && match[2].length == 11) {
+            return match[2];
+        } else {
+            return 'error';
+        }
+    }
+    let myId = getId(musicUrl);
+    console.log('myId : ',myId);
     return (
         <>
             <div className="circle">
                 <div className="inner__circle" style={{backgroundImage:`url(${lpImg})`}}></div>
             </div>
-            <embed src={musicUrl} loop="true" autostart="true" width="2" height="0" />
+            <iframe className="iframe" width="560" height="315" src={`https://www.youtube.com/embed/${myId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <div className="console">
                 <button className="prev"></button>
                 <button className="pause"></button>
